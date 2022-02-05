52dfced5e8527056b3682b37562a10536f99ca3e
Nobody <a9812315989@gmail.com>
2022-02-05 12:14:10 +0545

    Reduced the code for eventlistener

--- RPS.js
+++ RPS.js
@@ -1,94 +1,71 @@
-document.querySelector(".scoreMessage").textContent =
-  "Welcome to the game!";
+let userScore = 0;
+let computerScore = 0;
 
-  function computerPlay() {
-    let rps = Math.ceil(Math.random() * 3);
+let choiceList = document.querySelectorAll(".choose");
+choiceList.forEach(function (currentValue) {
+  currentValue.addEventListener("click", () => {
+    computerSelection = computerPlay();
+    playerSelection = currentValue.className.split(" ")[1];
 
-    if (rps === 1) return "rock";
-    if (rps === 2) return "paper";
-    if (rps === 3) return "scissor";
+    updateScore(playRound(playerSelection, computerSelection),playerSelection, computerSelection);
+
+  });
+});
+
+// ###################################################################################################################
+
+// Functions
+
+function updateScore(result, playerSelection, computerSelection) {  
+  // player lost
+  if (result == 0) {
+    document.querySelector(".computerScore").textContent = ++computerScore;
+    document.querySelector(".scoreMessage").textContent =
+      "You lost!! " + computerSelection + " beats " + playerSelection + ". ";
   }
-  function playRound(playerSelection, computerSelection) {
-    if (
-      (playerSelection == "rock" && computerSelection == "paper") ||
-      (playerSelection == "scissor" && computerSelection == "rock") ||
-      (playerSelection == "paper" && computerSelection == "scissor")
-    ) {
-      return 0;
 
-    } else if (
-      (playerSelection == "rock" && computerSelection == "scissor") ||
-      (playerSelection == "paper" && computerSelection == "rock") ||
-      (playerSelection == "scissor" && computerSelection == "paper")
-    ) {
-      return 1;
-    } else if (
-      (playerSelection == "rock" && computerSelection == "rock") ||
-      (playerSelection == "paper" && computerSelection == "paper") ||
-      (playerSelection == "scissor" && computerSelection == "scissor")
-      ) {
-	      return -1;
-	} else {
-		return null;
-	}
+  // player won
+  else if (result == 1) {
+    document.querySelector(".userScore").textContent = ++userScore;
+    document.querySelector(".scoreMessage").textContent =
+      "You Won!! " + playerSelection + " beats " + computerSelection + ". ";
+  }
+
+  // Tie
+  else {
+    document.querySelector(".scoreMessage").textContent =
+      "It's a tie. Computer chose the same. ";
+  }
 }
 
-let userScore = 0,
-computerScore = 0;
+function computerPlay() {
+  let rps = Math.ceil(Math.random() * 3);
 
-let rock = document.querySelector(".rock");
-let paper = document.querySelector(".paper");
-let scissor = document.querySelector(".scissor");
+  if (rps === 1) return "rock";
+  if (rps === 2) return "paper";
+  if (rps === 3) return "scissor";
+}
 
-
-rock.addEventListener("click", () => {
-	playerSelection = "rock";
-	computerSelection = computerPlay();
-	updateScore(
-		playRound(playerSelection, computerSelection),
-		playerSelection,
-		computerSelection
-		);
-	});
-		paper.addEventListener("click", () => {
-			playerSelection = "paper";
-			computerSelection = computerPlay();
-			updateScore(
-				playRound(playerSelection, computerSelection),
-				playerSelection,
-				computerSelection
-				);
-			});
-			scissor.addEventListener("click", () => {
-				playerSelection = "scissor";
-				computerSelection = computerPlay();
-				updateScore(
-					playRound(playerSelection, computerSelection),
-					playerSelection,
-					computerSelection
-					);
-				});
-				
-  function updateScore(result, playerSelection, computerSelection) {
-
-    // player lost
-    if (result == 0) {
-      document.querySelector(".computerScore").textContent = ++computerScore;
-      document.querySelector(".scoreMessage").textContent =
-        "You lost!! " + computerSelection + " beats " + playerSelection + ". ";
-    }
-
-    // player won
-    else if (result == 1) {
-      document.querySelector(".userScore").textContent = ++userScore;
-      document.querySelector(".scoreMessage").textContent =
-        "You Won!! " + playerSelection + " beats " + computerSelection + ". ";
-    }
-
-    // Draw
-    else {
-      document.querySelector(".scoreMessage").textContent =
-        "It's a tie. Computer chose the same. ";
-    }
+function playRound(playerSelection, computerSelection) {
+  if (
+    (playerSelection == "rock" && computerSelection == "paper") ||
+    (playerSelection == "scissor" && computerSelection == "rock") ||
+    (playerSelection == "paper" && computerSelection == "scissor")
+  ) {
+    return 0;
+  } else if (
+    (playerSelection == "rock" && computerSelection == "scissor") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissor" && computerSelection == "paper")
+  ) {
+    return 1;
+  } else if (
+    (playerSelection == "rock" && computerSelection == "rock") ||
+    (playerSelection == "paper" && computerSelection == "paper") ||
+    (playerSelection == "scissor" && computerSelection == "scissor")
+  ) {
+    return -1;
+  } else {
+    return null;
   }
-
+}
